856074c69fc16c03e37995b80a82882ff346f49b
eichelb4rt <maurice.wenig@gmail.com>
2022-11-01 15:26:09 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Wichtel random Leute mischen
+
+## Usage
+
+Jeder bekommt irgendwie random ein Geschenk. Gebt beim Einsammeln der Geschenke jeder Person eine Nummer. Diese Nummer klebt ihr an die Person und an das Geschenk. Man fängt bei 0 an zu zählen.
+
+```text
+python wichteln.py -h
+```
---
+++ wichteln.py
@@ -0,0 +1,18 @@
+import random
+import argparse
+from tabulate import tabulate
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Jeder bekommt irgendwie random ein Geschenk. Gebt beim Einsammeln der Geschenke jeder Person eine Nummer. Diese Nummer klebt ihr an die Person und an das Geschenk. Man fängt bei 0 an zu zählen.")
+    parser.add_argument("n", type=int, help="Anzahl der Wichtler")
+    args = parser.parse_args()
+    players = list(range(args.n))
+    random.shuffle(players)
+
+    presents = [[player, players[(i + 1) % len(players)]] for i, player in enumerate(players)]
+    print(tabulate(presents, headers=["Person", "bekommt Geschenk"], tablefmt="grid"))
+
+
+if __name__ == "__main__":
+    main()
